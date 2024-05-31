1f359e82a7c88a58d3c6c8856ea6ed870dcca06e
snavale2 <snavale@binghamton.edu>
2024-05-30 23:34:35 -0400

    feat: Added Model code

---
+++ movies/src/main/java/com/moviestation/movies/MovieModel.java
@@ -0,0 +1,30 @@
+package com.moviestation.movies;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieModel {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<ReviewModel> reviewModelIds;
+
+}
