7b03b5c0a91330b95c4ea12295c20504ffb2aa06
snavale2 <snavale@binghamton.edu>
2024-06-01 14:23:03 -0400

    Created react app and installed dependencies

---
+++ movie-client-v1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import api from './api/axiosConfig';
+import { useState } from 'react';
+
+function App() {
+
+  const [movies, setMovies] = useState();
+  
+  return (
+    
+  );
+}
+
+export default App;
